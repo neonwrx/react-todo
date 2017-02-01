fa91157a95b8d440c547489f0b2529122d159487
neonwrx <rolexxx91@gmail.com>
2017-02-01 22:37:31 +0200

    Add playground for spread operator

---
+++ playground/spread.js
@@ -0,0 +1,30 @@
+// function add (a, b) {
+//   return a + b;
+// }
+//
+// console.log(add(3, 1));
+//
+// var toAdd = [9, 5];
+// console.log(add(...toAdd));
+
+// var groupA = ['Jen', 'Cory'];
+// var groupB = ['Vikram'];
+// var final = [...groupB, 3, ...groupA];
+//
+// console.log(final);
+
+var person = ['Andrew', 25];
+var personTwo = ['Jen', 29];
+
+function great (name, age) {
+  console.log('Hi ' + name + ', you are ' + age);
+}
+great(...person);
+great(...personTwo);
+
+var names = ['Mike', 'Ben'];
+var final = ['Andrew', ...names];
+
+final.forEach(function(name) {
+  console.log('Hi ' + name);
+});
